3a87e4a057d9c2636b9bbbd27b62568f8c469979
unknown <Jessiealcalde28@gmail.com>
2025-05-14 14:50:17 -0800

    add linkin

--- src/components/sociallinks.jsx
+++ src/components/sociallinks.jsx
@@ -4,6 +4,7 @@ export default function SocialLinks() {
         <h2>Connect with Me</h2>
         <ul>
           <li><a href="https://github.com/Jessiealcalde28" target="_blank">GitHub</a></li>
+          <li><a href="https://www.linkedin.com/in/jessie-alcalde-09151b361/" target="_blank">LinkIn</a></li>
           <li><a >09946430558</a></li>
           <li><a >jessiealcalde28@gmail.com</a></li>
           <li><a href="https://www.facebook.com/jessiealcalde1028">Facebook</a></li>
